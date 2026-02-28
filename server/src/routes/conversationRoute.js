509671b3031086c7958f3770fc0927a5d52dca97
Trịnh Huỳnh <trinhpro2304@gmail.com>
2026-02-28 23:16:41 +0700

    add markAsSeen func

--- server/src/routes/conversationRoute.js
+++ server/src/routes/conversationRoute.js
@@ -3,6 +3,7 @@ import {
   createConversation,
   getConversations,
   getMessages,
+  markAsSeen,
 } from "../controllers/conversationController.js";
 import { checkFriendship } from "../middlewares/friendMiddleware.js";
 
@@ -13,5 +14,5 @@ router.post("/", checkFriendship, createConversation);
 
 router.get("/", getConversations);
 router.get("/:conversationId/messages", getMessages);
-
+router.patch("/:conversationId/seen", markAsSeen);
 export default router;
