8d400016992c848972240b005ee5042649543e35
Trịnh Huỳnh <trinhpro2304@gmail.com>
2026-02-28 23:21:56 +0700

    add api markAsSeen from client

--- client/src/services/chatService.ts
+++ client/src/services/chatService.ts
@@ -51,4 +51,9 @@ export const chatService = {
     });
     return res.data.message;
   },
+
+  async markAsSeen(conversationId: string) {
+    const res = await api.patch(`/conversations/${conversationId}/seen`);
+    return res.data;
+  },
 };
