17dfd9f3174ce00822eb4f73b2311944a0ae6001
Trịnh Huỳnh <trinhpro2304@gmail.com>
2026-03-04 21:13:55 +0700

    handle load more message

--- client/src/components/chat/ChatWindowBody.tsx
+++ client/src/components/chat/ChatWindowBody.tsx
@@ -2,12 +2,14 @@ import ChatWelcomeScreen from "@/components/chat/ChatWelcomeScreen";
 import MessageItem from "@/components/chat/MessageItem";
 import { useChatStore } from "@/stores/useChatStore";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import InfinitScroll from "react-infinite-scroll-component";
 
 const ChatWindowBody = () => {
   const {
     activeConversationId,
     conversations,
     messages: allMessages,
+    fetchMessages,
   } = useChatStore();
 
   const [lastMessageStatus, setLastMessageStatus] = useState<
@@ -15,13 +17,17 @@ const ChatWindowBody = () => {
   >("delivered");
 
   const messages = allMessages[activeConversationId!]?.items ?? [];
+  const reversedMessages = [...messages].reverse();
+  const key = `chat-scroll-${activeConversationId}`;
 
+  const hasMore = allMessages[activeConversationId!]?.hasMore ?? false; // true/false
   // lấy ra convo được đang được chọn
   const selectedConvo = conversations.find(
     (c) => c._id === activeConversationId,
   );
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const lastMessage = selectedConvo?.lastMessage;
@@ -36,9 +42,45 @@ const ChatWindowBody = () => {
     if (!messagesEndRef.current) {
       return;
     }
-
     messagesEndRef.current.scrollIntoView({ block: "center" });
   }, [activeConversationId]);
+
+  const fetchMoreMessages = async () => {
+    if (!activeConversationId) {
+      return;
+    }
+    try {
+      await fetchMessages(activeConversationId);
+    } catch (error) {
+      console.error("Lỗi fetchMoreMessages", error);
+    }
+  };
+
+  const handleScrollSave = () => {
+    const container = containerRef.current;
+    if (!container || !activeConversationId) {
+      return;
+    }
+    sessionStorage.setItem(
+      key,
+      JSON.stringify({
+        scrollTop: container.scrollTop,
+        scrollHeight: container.scrollHeight,
+      }),
+    );
+  };
+
+  useLayoutEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const item = sessionStorage.getItem(key);
+    if (item) {
+      const { scrollTop } = JSON.parse(item);
+      requestAnimationFrame(() => {
+        container.scrollTop = scrollTop;
+      });
+    }
+  }, [messages.length]);
 
   if (!selectedConvo) {
     return <ChatWelcomeScreen />;
@@ -54,21 +96,40 @@ const ChatWindowBody = () => {
 
   return (
     <div className="p-4 bg-primary-foreground h-full flex flex-col overflow-hidden">
-      <div className="flex flex-col h-full overflow-y-auto overflow-x-hidden beautifull-scrollbar">
-        {messages.map((m, i) => {
-          return (
-            <MessageItem
-              key={m._id ?? i}
-              message={m}
-              index={i}
-              messages={messages}
-              selectedConvo={selectedConvo}
-              lastMessageStatus={lastMessageStatus}
-            />
-          );
-        })}
-
+      <div
+        ref={containerRef}
+        id="scrollableDiv"
+        onScroll={handleScrollSave}
+        className="flex flex-col-reverse h-full overflow-y-auto overflow-x-hidden beautifull-scrollbar"
+      >
         <div ref={messagesEndRef}></div>
+
+        <InfinitScroll
+          dataLength={messages.length}
+          next={fetchMoreMessages}
+          hasMore={hasMore}
+          scrollableTarget="scrollableDiv"
+          loader={<p>Đang tải...</p>}
+          inverse={true}
+          style={{
+            display: "flex",
+            flexDirection: "column-reverse",
+            overflow: "visible",
+          }}
+        >
+          {reversedMessages.map((m, i) => {
+            return (
+              <MessageItem
+                key={m._id ?? i}
+                message={m}
+                index={i}
+                messages={reversedMessages}
+                selectedConvo={selectedConvo}
+                lastMessageStatus={lastMessageStatus}
+              />
+            );
+          })}
+        </InfinitScroll>
       </div>
     </div>
   );
--- client/src/components/chat/MessageItem.tsx
+++ client/src/components/chat/MessageItem.tsx
@@ -19,92 +19,87 @@ const MessageItem = ({
   selectedConvo,
   lastMessageStatus,
 }: MessageItemProps) => {
-  const prev = messages[index - 1];
-  const next = messages[index + 1];
-
-  const isGroupBreak =
-    index === 0 ||
-    message.senderId !== prev.senderId ||
-    // thời gian tin mới nhất và tin sau đó mà lớn hơn 5ph thì tạo mới
-    new Date(message.createdAt).getTime() -
-      new Date(prev?.createdAt || 0).getTime() >
-      60000;
+  const prev = index + 1 < messages.length ? messages[index + 1] : undefined;
 
   const isShowTime =
-    new Date(next?.createdAt).getTime() -
-      new Date(message.createdAt).getTime() >
-      60000 || message.senderId !== next?.senderId;
+    index === 0 ||
+    new Date(message.createdAt).getTime() -
+      new Date(prev?.createdAt || 0).getTime() >
+      300000; //5ph
+
+  const isGroupBreak = isShowTime || message.senderId !== prev?.senderId;
 
   const paricipant = selectedConvo.participants.find(
     (u) => u._id.toString() === message.senderId.toString(),
   );
 
   return (
-    <div
-      className={cn(
-        "flex gap-2 message-bounce mt-1", // message-bounce hiệu ứng animation mờ và nhỏ -> rõ và lớn dần
-        message.isOwn ? "justify-end" : "justify-start",
-      )}
-    >
-      {/* Avatar */}
-      {!message.isOwn && (
-        <div className="w-8">
-          {isGroupBreak && (
-            <UserAvatar
-              name={paricipant?.displayName ?? "Moji"}
-              type="chat"
-              avatarUrl={paricipant?.avatarUrl ?? undefined}
-            />
-          )}
-        </div>
-      )}
-
-      {/* Content tin nhắn */}
-      <div
-        className={cn(
-          "max-w-xs lg:max-w-md space-y-0 flex flex-col",
-          message.isOwn ? "items-end" : "items-start",
-        )}
-      >
-        <Card
+    <>
+      {/* Time */}
+      {isShowTime && (
+        <span
           className={cn(
-            "p-3",
-            message.isOwn ? "chat-bubble-sent" : "bg-chat-bubble-received",
+            "flex justify-center text-xs text-muted-foreground px-1",
+            message._id !== selectedConvo.lastMessage?._id ? "mb-4" : "",
           )}
         >
-          <p className="text-sm leading-relaxed break-words">
-            {message.content}
-          </p>
-        </Card>
-
-        {/* Time */}
-        {isShowTime && (
-          <span
-            className={cn(
-              "text-sm text-muted-foreground px-1",
-              message._id !== selectedConvo.lastMessage?._id ? "mb-4" : "",
+          {formatMessageTime(new Date(message.createdAt))}
+        </span>
+      )}
+      <div
+        className={cn(
+          "flex gap-2 message-bounce mt-1", // message-bounce hiệu ứng animation mờ và nhỏ -> rõ và lớn dần
+          message.isOwn ? "justify-end" : "justify-start",
+        )}
+      >
+        {/* Avatar */}
+        {!message.isOwn && (
+          <div className="w-8">
+            {isGroupBreak && (
+              <UserAvatar
+                name={paricipant?.displayName ?? "Moji"}
+                type="chat"
+                avatarUrl={paricipant?.avatarUrl ?? undefined}
+              />
             )}
-          >
-            {formatMessageTime(new Date(message.createdAt))}
-          </span>
+          </div>
         )}
 
-        {/* Seen / delivered */}
-        {message.isOwn && message._id === selectedConvo.lastMessage?._id && (
-          <Badge
-            variant="outline"
+        {/* Content tin nhắn */}
+        <div
+          className={cn(
+            "max-w-xs lg:max-w-md space-y-0 flex flex-col",
+            message.isOwn ? "items-end" : "items-start",
+          )}
+        >
+          <Card
             className={cn(
-              "text-xs px-1.5 py-0.5 h-4 border-0",
-              lastMessageStatus === "seen"
-                ? "bg-primary/20 text-primary"
-                : "bg-muted text-muted-foreground",
+              "p-3",
+              message.isOwn ? "chat-bubble-sent" : "bg-chat-bubble-received",
             )}
           >
-            {lastMessageStatus}
-          </Badge>
-        )}
+            <p className="text-sm leading-relaxed break-words">
+              {message.content}
+            </p>
+          </Card>
+
+          {/* Seen / delivered */}
+          {message.isOwn && message._id === selectedConvo.lastMessage?._id && (
+            <Badge
+              variant="outline"
+              className={cn(
+                "text-xs px-1.5 py-0.5 h-4 border-0",
+                lastMessageStatus === "seen"
+                  ? "bg-primary/20 text-primary"
+                  : "bg-muted text-muted-foreground",
+              )}
+            >
+              {lastMessageStatus}
+            </Badge>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
