9a2f43bc77df10ae93c835bfbc9fb4897d87cf7f
Trịnh Huỳnh <trinhpro2304@gmail.com>
2026-03-04 22:26:37 +0700

    add api search user by username

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 export const authMe = async (req, res) => {
   try {
@@ -14,6 +14,25 @@ export const authMe = async (req, res) => {
   }
 };
 
-export const test = (req, res) => {
-  res.sendStatus(204);
+export const searchUserByUsername = async (req, res) => {
+  try {
+    const { username } = req.query;
+    if (!username || username.trim() === "") {
+      return res.status(400).json({ message: "Không nhận được username" });
+    }
+
+    const user = await User.findOne({ username }).select(
+      "_id displayName username avatarUrl",
+    );
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error(
+      "Lỗi khi gọi searchUserByUsername [userController.js]",
+      error,
+    );
+    return res.status(500).json({
+      message: "Lỗi hệ thống",
+    });
+  }
 };
